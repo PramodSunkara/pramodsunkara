10bb14f02e8ee049065a9057f7ad4d12be8a8e96
Pramod Sunkara <sunkara.pramod@gmail.com>
2025-12-27 20:10:52 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       base: "/pramodsunkuara/"
+      build: { outDir: 'dist' }
     },
   },
 }));
